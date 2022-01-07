f3d2fda027173492f21a483e4d3e8ebfd835ed9c
Mohammed Mujahid <mujahid9619@gmail.com>
2022-01-07 08:43:38 +0530

    assignments practice

--- practice.js
+++ practice.js
@@ -1,53 +1,104 @@
 //this is for practing
-
 // we have created more before commit the command promp
-
 //net we can check the the comming process is going on
-
 //this is last message
-
 //after submit the code to git hub i am adding more line to check the text
-
 // this matter is saving in the new branch which i created new one
-
-
 // i had put another headding in matter
-
 
 console.log("i am mujahid");
 
 //assignment one
-
 var DOB = 1988;
 var PY = 2022;
-
 var age = PY-DOB;
-
 console.log(age);
 
 // assignment two
-
 var age = 35;
 console.log(age.toString());
-
 var age ="38";
 console.log(Number(age));
 
 // assignment three
-
 var a = "coding" + " " + "is";
 var b = "easy";
-
 var letter = (a +" "+ b);
-
 console.log(letter);
 
 // assignment four
-
 var english = 98;
 var telugu = 95;
 var hindi = 93;
 var totalmarks = english+telugu+hindi;
-
 console.log(totalmarks);
+
+// assignment Five
+var length = 4;
+var height = 2;
+var perimeter = 2*length + 2*height;
+var area = length*height;
+console.log(perimeter);
+console.log(area);
+
+// assignment Six
+
+var location = "2nd Floor, 2nd Street, Panjagutta, ";
+var city = "Hyderabad, ";
+var state = "Telangana.";
+
+console.log(location + city + state);
+
+// assignment Seven
+
+var months = 26;
+console.log(months % 12);
+
+// assignment Eight
+
+var principleamount = 1000;
+var interestpercent = 6.5;
+var time = 12;
+
+var interst = principleamount%interestpercent;
+console.log(interst * time);
+
+// assignment Nine
+
+var stu1 = 180;
+var stu2 = 150;
+var stu3 = 140;
+var stu4 = 180;
+var stu5 = 160;
+var stu6 = 150;
+
+var average = stu1+stu2+stu3+stu4+stu5+stu6;
+console.log(average/6);
+
+// assignment Ten
+
+var pprice1 = 25;
+var pprice2 = 48;
+var pprice3 = 45
+
+var qnty = (pprice1*2 + pprice2*8 + pprice3*6);
+
+console.log(qnty);
+
+// assignment Eleven
+
+var itemprice1 = 80;
+var itemprice2 = 140;
+
+var qnty = (itemprice1*6 + itemprice2*3);
+
+var fivepercent = (qnty%100);
+
+
+
+
+
+
+
+
 
